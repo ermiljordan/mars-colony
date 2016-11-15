52a5562a1b4dec599fa711391e2a9c5474d4eb8f
ermiljordan <ermiljordan64@gmail.com>
2016-11-14 16:52:07 -0800

    deleted and renewed

---
+++ src/app/models/index.ts
@@ -0,0 +1,32 @@
+export class Encounter {
+  constructor(
+    public id: number,
+    public date: string,
+    public colonist_id: number,
+    public atype: string,
+    public action: string
+  ){}
+}
+export class Colonist {
+  constructor(
+    public name: string,
+    public job: Job,
+    public id: number,
+    public age: number
+  ){}
+}
+export class Job {
+  constructor(
+    public name: string,
+    public id: number,
+    public description: string,
+  ){}
+}
+export class Alien {
+  constructor(
+    public type: string,
+    public submitted_by: string,
+    public id: number,
+    public description: string,
+  ){}
+}
